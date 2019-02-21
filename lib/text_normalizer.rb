334ad6d84827ef2adc81501b0da9e340fc8ea375
Von Christian Halip <vc.halip@gmail.com>
2019-02-21 09:18:01 +0800

    initialize repository

---
+++ lib/text_normalizer.rb
@@ -0,0 +1,6 @@
+require "text_normalizer/version"
+
+module TextNormalizer
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/text_normalizer/version.rb
@@ -0,0 +1,3 @@
+module TextNormalizer
+  VERSION = "0.1.0"
+end
